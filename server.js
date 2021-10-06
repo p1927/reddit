3704e2850ceb734e4ec3a766f51bfc90c6e4d304
prat <99pratyush@gmail.com>
2021-10-06 09:16:13 +0530

    working application

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/reddit'));
+app.use(express.static(__dirname + '/dist/Reddit'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/reddit/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/Reddit/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
